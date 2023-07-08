dfa54e48bf9db24d796b067309b96d53c76fd22e
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-07-08 17:25:49 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# Importing the heapq module for Priority Queue
+import heapq
+
+def calculate_distances(graph, starting_vertex):
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[starting_vertex] = 0
+
+    pq = [(0, starting_vertex)]
+    while len(pq) > 0:
+        current_distance, current_vertex = heapq.heappop(pq)
+
+        # Nodes can get added to the priority queue multiple times. We only
+        # process a vertex the first time we remove it from the priority queue.
+        if current_distance > distances[current_vertex]:
+            continue
+
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+
+            # Only consider this new path if it's better than any path we've
+            # already found.
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+    
+    return distances
+
+# Here's our graph represented as an adjacency list
+graph = {
+    'A': {'B': 1, 'C': 3},
+    'B': {'A': 1, 'C': 2, 'D': 1},
+    'C': {'A': 3, 'B': 2, 'D': 1},
+    'D': {'B': 1, 'C': 1},
+}
+
+print(calculate_distances(graph, 'A'))  # {'A': 0, 'B': 1, 'C': 2, 'D': 2}
