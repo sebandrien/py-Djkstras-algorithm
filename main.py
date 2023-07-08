93e84baa70e32a273075eed97df6f391c7bee210
Sebastian <129107648+sebandrien@users.noreply.github.com>
2023-07-08 17:40:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,32 +5,39 @@ def calculate_distances(graph, starting_vertex):
     distances = {vertex: float('infinity') for vertex in graph}
     distances[starting_vertex] = 0
 
+    paths = {vertex: [] for vertex in graph}
+    paths[starting_vertex] = [starting_vertex]
+
     pq = [(0, starting_vertex)]
     while len(pq) > 0:
         current_distance, current_vertex = heapq.heappop(pq)
 
-        # Nodes can get added to the priority queue multiple times. We only
-        # process a vertex the first time we remove it from the priority queue.
         if current_distance > distances[current_vertex]:
             continue
 
         for neighbor, weight in graph[current_vertex].items():
             distance = current_distance + weight
 
-            # Only consider this new path if it's better than any path we've
-            # already found.
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
+                paths[neighbor] = paths[current_vertex] + [neighbor]
                 heapq.heappush(pq, (distance, neighbor))
-    
-    return distances
 
-# Here's our graph represented as an adjacency list
-graph = {
+    return distances, paths
+
+graph_1 = {
     'A': {'B': 1, 'C': 3},
     'B': {'A': 1, 'C': 2, 'D': 1},
     'C': {'A': 3, 'B': 2, 'D': 1},
     'D': {'B': 1, 'C': 1},
 }
 
-print(calculate_distances(graph, 'A'))  # {'A': 0, 'B': 1, 'C': 2, 'D': 2}
+distances, paths = calculate_distances(graph_1, 'A')
+print("Shortest distances:", distances)
+
+# Find the farthest node
+farthest_node = max(distances, key=distances.get)
+print("Farthest node:", farthest_node)
+
+# Get the shortest path to the farthest node
+print("Shortest path from starting node to farthest node:", paths[farthest_node])
